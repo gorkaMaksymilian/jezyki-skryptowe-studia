7043625c5e99bf9b2542d4e07edaa08df6b09ee1
MichalGregorczyk <45073501+MichalGregorczyk@users.noreply.github.com>
2018-11-28 19:30:54 +0100

    Update findDatabase.py

--- findDatabase.py
+++ findDatabase.py
@@ -1,4 +1,6 @@
 import requests
+import logging
+logging.basicConfig(filename='main.log',level='INFO',format='%(asctime)s: %(levelname)s: %(message)s')
 
 url="http://www.ttss.krakow.pl/internetservice/geoserviceDispatcher/services/stopinfo/stops?left=-648000000&bottom=-324000000&right=648000000&top=324000000"
 przystanki = requests.get(url)
@@ -17,12 +19,14 @@ def findInDatabase(przystanekNazwa):
         for x in przystanki.json()["stops"]:
             if x["name"].lower() == przystanekNazwa:
                 print('Znaleziono przystanek: '+x["name"])
+                logging.warning('  User podal prawidlowa nazwe przystanku')
                 nazwa=x["name"]
                 numerPrzystanku=x["shortName"]
                 czyWystapil=True
                 break
         if czyWystapil==False:
             print("Błędna nazwa przystanku")
+            logging.warning('  User podal bledna nazwe przystanku')
             loop=1
 
     return {'a':"http://www.ttss.krakow.pl/#?stop="+numerPrzystanku+"&mode=departure","b":nazwa}
