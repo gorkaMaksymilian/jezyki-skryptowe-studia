d15e6325c258f2405b0b258062b5f96875763f1d
MichalGregorczyk <45073501+MichalGregorczyk@users.noreply.github.com>
2018-11-28 19:31:27 +0100

    Update syntaxCorrection.py

--- syntaxCorrection.py
+++ syntaxCorrection.py
@@ -1,9 +1,13 @@
+import logging
+logging.basicConfig(filename='main.log',level='INFO',format='%(asctime)s: %(levelname)s: %(message)s')
+
 def inputCorrection():
     liczbaPrzystankow=input("Podaj liczbe przystanków do wyswietlenia (>0 && <6): ")
     sprawdzanie=liczbaPrzystankow.isdigit()
     if(sprawdzanie == False):
         while (sprawdzanie == False):
             print('!!!Wpisz liczbe!!!')
+            logging.warning('  User podal litere zamiast liczby')
             liczbaPrzystankow=input("Podaj liczbe przystanków do wyswietlenia (>0 && <6): ")
             sprawdzanie=liczbaPrzystankow.isdigit()
     
@@ -12,6 +16,7 @@ def inputCorrection():
     if(liczbaPrzystankow>6 or liczbaPrzystankow <= 0):
         while (liczbaPrzystankow>6 or liczbaPrzystankow <= 0):
             print('!!!Popraw liczbe przystanków!!!\n')
+            logging.warning('  User nie podal poprawnej liczby przystanku')
             liczbaPrzystankow=input("Podaj liczbe przystanków do wyswietlenia (>0 && <6): ")
             liczbaPrzystankow=int(liczbaPrzystankow)
     
