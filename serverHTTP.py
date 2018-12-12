c4eeab453f2619ffba697ccd6e6152046ff3fded
Maksymilian Górka <44338133+gorkaMaksymilian@users.noreply.github.com>
2018-12-12 16:17:40 +0100

    Sprawdzanie czy port jest wolny

--- serverHTTP.py
+++ serverHTTP.py
@@ -2,6 +2,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import codecs
 import os
 import sys
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sys.tracebacklimit=0
  
  
@@ -27,8 +30,19 @@ class StaticServer(BaseHTTPRequestHandler):
             self.wfile.write(html)
 
  
-
 port=8000
+czyWolny=True
+
+while czyWolny:
+    result = sock.connect_ex(('127.0.0.1',port))
+    if result == 0:
+       print ("\nPort {} jest zajety.\nPodaj inny numer portu:".format(port))
+       port=int(input())
+    else:
+       print ("Port jest wolny.\nŁącze sie z 127.0.0.1:{}".format(port))
+       czyWolny=False
+       
+
 server_address = ('', port)
 httpCSS = HTTPServer(server_address, StaticServer)
 print('Serwer działa: localhost na porcie {}'.format(port))
@@ -37,4 +51,3 @@ try:
 	httpCSS.serve_forever()
 except KeyboardInterrupt:
 	pass
-    
